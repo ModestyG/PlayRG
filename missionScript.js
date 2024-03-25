9a9de1f35ce46bbd2929246f14018ede909b577f
Julia <julia.green@skola.taby.se>
2024-03-25 13:11:57 +0100

    Adding contacts and started dialog box

--- missionScript.js
+++ missionScript.js
@@ -1,19 +1,46 @@
+class Contact {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
 let missionDiv = document.getElementById("mission-div");
 let phoneButton = document.getElementById("phone-button");
+let phoneDiv = document.getElementById("phone-div");
 let crypto = 50;
 let currentMission = "";
+let contacts = [new Contact("Test")];
 
 //Load saved values
 if (localStorage.length) {
   crypto = Number(localStorage.getItem("crypto"));
 }
 
-function callAction() {
+function openPhone() {
   phoneButton.classList.add("hidden");
-  let phoneDiv = document.createElement("div");
-  phoneDiv.id = "phone-div";
-  phoneDiv.innerHTML = "<button onclick='addCrypto()'></button>";
-  missionDiv.appendChild(phoneDiv);
+  phoneDiv.style.display = "flex";
+  phoneDiv.innerHTML =
+    "<button onclick='closePhone()' id='close-phone-button'>x</button>";
+  contacts.forEach((contact) => {
+    btn = document.createElement("button");
+    btn.onclick = function () {
+      call(contact);
+    };
+    btn.innerHTML = contact.name;
+    phoneDiv.appendChild(btn);
+  });
+}
+
+function call(contact) {
+  phoneDiv.innerHTML = "";
+  let dialogBox = document.createElement("div");
+  dialogBox.id = "dialog-box";
+  phoneDiv.appendChild(dialogBox);
+}
+
+function closePhone() {
+  phoneButton.classList.remove("hidden");
+  phoneDiv.style.display = "none";
 }
 
 function addCrypto() {
